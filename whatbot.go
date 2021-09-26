15ff451177d16f89bd248f682bd1abb8d653beab
Ben Grant <benjamin.j.grant@gmail.com>
2021-09-26 12:23:49 -0700

    Take token, icons directory, and guild ID from flags

--- whatbot.go
+++ whatbot.go
@@ -2,42 +2,55 @@ package main
 
 import (
 	"encoding/base64"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
-	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	var token, guildId, iconsDir string
+	flag.StringVar(&token, "t", "", "discord token")
+	flag.StringVar(&guildId, "g", "", "ID of the guild to modify")
+	flag.StringVar(&iconsDir, "i", "icons", "directory to choose an icon from")
+	flag.Parse()
+	if token == "" || guildId == "" || iconsDir == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	discord, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "error with discord initialization:", err)
-		return
+		os.Exit(2)
 	}
 	
 	err = discord.Open()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "error opening discord connection:", err)
-		return
+		os.Exit(2)
 	}
 
 	fmt.Println("connected to discord")
 
-	files, err := ioutil.ReadDir("icons")
+	files, err := ioutil.ReadDir(iconsDir)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "error reading `icons` directory:", err)
+		fmt.Fprintf(os.Stderr, "error reading `%s` directory: %v\n", iconsDir, err)
+		os.Exit(2)
 	}
 
 	rand.Seed(time.Now().UnixNano())
-	filename := "icons/" + files[rand.Intn(len(files))].Name()
+	filename := filepath.Join(iconsDir, files[rand.Intn(len(files))].Name())
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error reading `%s`: %v\n", filename, err)
-		return
+		os.Exit(2)
 	}
 
 	fmt.Printf("using `%s`\n", filename)
@@ -45,13 +58,11 @@ func main() {
 	mime := http.DetectContentType(bytes)
 	dataUrl := fmt.Sprintf("data:%s;base64,%s", mime, base64.StdEncoding.EncodeToString(bytes))
 
-	fmt.Println("made data url")
-
-	_, err = discord.GuildEdit("891527327144611901", discordgo.GuildParams{
+	_, err = discord.GuildEdit(guildId, discordgo.GuildParams{
 		Icon: dataUrl,
 	})
 	if err != nil {
 		fmt.Println("error setting icon:", err)
-		return
+		os.Exit(2)
 	}
 }
