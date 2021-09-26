4c186d697c3f07ebeebdaceb83f03bb72f6ca43b
Ben Grant <benjamin.j.grant@gmail.com>
2021-09-26 11:14:00 -0700

    Actually change icon

--- whatbot.go
+++ whatbot.go
@@ -1,28 +1,54 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
-	fmt.Println("hello")
 	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
 	if err != nil {
-		fmt.Println("error with discord initialization:", err)
+		fmt.Fprintln(os.Stderr, "error with discord initialization:", err)
 		return
 	}
 	
 	err = discord.Open()
 	if err != nil {
-		fmt.Println("error opening discord connection:", err)
+		fmt.Fprintln(os.Stderr, "error opening discord connection:", err)
 		return
 	}
 
+	fmt.Println("connected to discord")
+
+	files, err := ioutil.ReadDir("icons")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error reading `icons` directory:", err)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	filename := "icons/" + files[rand.Intn(len(files))].Name()
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error reading `%s`: %v\n", filename, err)
+		return
+	}
+
+	fmt.Printf("using `%s`\n", filename)
+
+	mime := http.DetectContentType(bytes)
+	dataUrl := fmt.Sprintf("data:%s;base64,%s", mime, base64.StdEncoding.EncodeToString(bytes))
+
+	fmt.Println("made data url")
+
 	_, err = discord.GuildEdit("891527327144611901", discordgo.GuildParams{
-		Icon: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAADElEQVQI12NgyP4PAAHZAWuZrByGAAAAAElFTkSuQmCC",
+		Icon: dataUrl,
 	})
 	if err != nil {
 		fmt.Println("error setting icon:", err)
